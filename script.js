eaf166fd17763da7d96dbc2b030bc7266ca1c08f
BernardoPC-Dev <bernardonbotto@outlook.com>
2025-03-26 15:55:31 -0300

    Mudanças gerais

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.addEventListener('scroll', function () {
     const boxes = document.querySelectorAll('.lang');
   
     boxes.forEach(function (box) {
-      const boxPosition = box.getBoundingClientRect().top; // posição da caixa em relação ao topo da tela
-      const screenPosition = window.innerHeight / 1; // onde a caixa vai aparecer (ajuste conforme necessário)
+      const boxPosition = box.getBoundingClientRect().top;
+      const screenPosition = window.innerHeight;
   
       if (boxPosition < screenPosition) {
         box.classList.add('visible');
