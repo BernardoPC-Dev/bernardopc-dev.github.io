4ce88754f70ec04f76d79057fd119fa8ffb53735
BernardoPC-Dev <bernardonbotto@outlook.com>
2025-03-25 22:29:59 -0300

    Transições

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function goToProg() {
 function goToCris() {
     document.getElementById("cris").scrollIntoView({ behavior: "smooth" });
 }
+document.addEventListener('scroll', function () {
+    const boxes = document.querySelectorAll('.lang');
+  
+    boxes.forEach(function (box) {
+      const boxPosition = box.getBoundingClientRect().top; // posição da caixa em relação ao topo da tela
+      const screenPosition = window.innerHeight / 1.3; // onde a caixa vai aparecer (ajuste conforme necessário)
+  
+      if (boxPosition < screenPosition) {
+        box.classList.add('visible');
+      } else {
+        box.classList.remove('visible');
+      }
+    });
+  });
+  
